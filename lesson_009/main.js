ef56d6e3d2d78fc49981fba808d4800e9a0cac3b
阿莫 <>
2022-06-06 19:43:05 +0800

    update lesson_009

--- lesson_009/main.js
+++ lesson_009/main.js
@@ -8,26 +8,44 @@ let gl,
 let gGridShader, gGridModal;
 
 const vertexShader = `#version 300 es
-        in vec3 a_position;
+        in vec4 a_position;
+        in vec3 a_norm;
         in vec2 a_uv;
 
         uniform mat4 uPMatrix;
         uniform mat4 uMVMatrix;
         uniform mat4 uCameraMatrix;
+
+        uniform vec3 uColor[6];
+		uniform float uTime;
+
+        vec3 warp(vec3 p){
+			//return p + 0.2 * abs(cos(uTime*0.002)) * a_norm;
+			//return p + 0.5 * abs(cos(uTime*0.003 + p.y)) * a_norm;
+			return p + 0.5 * abs(cos(uTime*0.003 + p.y*2.0 + p.x*2.0 + p.z)) * a_norm;
+		}
         
+        out lowp vec4 color;
         out vec2 texCoord;
         void main(){
             texCoord = a_uv;
-            gl_Position = uPMatrix * uCameraMatrix *  uMVMatrix * vec4(a_position,1.0);
+            
+            color = vec4(uColor[ int(a_position.w) ],1.0);
+
+            gl_Position = uPMatrix * uCameraMatrix *  uMVMatrix * vec4(warp(a_position.xyz), 1.0);
         }
     `;
 const fragmentShader = `#version 300 es
         precision mediump float;
+
         out vec4 finalColor;
+
+        in vec4 color;
         in highp vec2 texCoord;
         uniform sampler2D uMainTex;
         void main(){
-            finalColor = texture(uMainTex,texCoord);
+            // finalColor = texture(uMainTex,texCoord);
+            finalColor = mix(color,texture(uMainTex,texCoord),0.8f);
         }
     `;
 const main = () => {
@@ -40,10 +58,11 @@ const main = () => {
     gl.loadTexture("tex001", document.getElementById("img"));
 
     gGridShader = new GridAxisShader(gl, gCamera.projectionMatrix);
-    gGridModal = Primatives.GridAxis.createModal(gl, true);
+    gGridModal = Primatives.GridAxis.createModal(gl, false);
 
     gShader = new TestShader(gl, gCamera.projectionMatrix).setTexture(gl.mTextureCache["tex001"]);
-    gModal = Primatives.MultiQuad.createModal(gl);
+
+    gModal = Primatives.Cube.createModal(gl);
     gModal.setPosition(0, 0.6, 0);
 
     gRLoop = new RenderLoop(onRender, 30).start();
@@ -56,6 +75,7 @@ function onRender(dt) {
     gGridShader.activate().setCameraMatrix(gCamera.viewMatrix).renderModal(gGridModal.preRender());
 
     gShader.activate().preRender().setCameraMatrix(gCamera.viewMatrix)
+        .setTime(performance.now())
         .renderModal(gModal.preRender());
 
 }
@@ -64,7 +84,12 @@ class TestShader extends Shader {
     constructor(gl, pMatrix) {
         super(gl, vertexShader, fragmentShader);	//Call the base class constructor which will setup most of what we need
 
+        this.uniformLoc.time = gl.getUniformLocation(this.program, "uTime");
+        var uColor = gl.getUniformLocation(this.program, "uColor");
+        gl.uniform3fv(uColor, new Float32Array(GLUtil.rgbArray("#FF0000", "00FF00", "0000FF", "909090", "C0C0C0", "404040")));
+
         this.setPerspective(pMatrix);
+        this.mainTexture = -1;
 
         gl.useProgram(null); //Done setting up shader
     }
@@ -74,10 +99,14 @@ class TestShader extends Shader {
         return this;
     }
 
+    setTime(t) {
+        this.gl.uniform1f(this.uniformLoc.time, t); return this;
+    }
+
     preRender() {
         this.gl.activeTexture(this.gl.TEXTURE0);
-        this.gl.bindTexture(this.gl.TEXTURE_2D,this.mainTexture);
-        this.gl.uniform1i(this.uniformLoc.mainTexture,0);
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.mainTexture);
+        this.gl.uniform1i(this.uniformLoc.mainTexture, 0);
         return this;
     }
 }
